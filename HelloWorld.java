06652bbe62977476b7fce49cfb81623f7851c668
Ryga171 <83324943+Ryga171@users.noreply.github.com>
2022-04-12 16:49:37 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello world!");
-    System.out.println("New java program");
+    System.out.println("New java program by ryga");
   }
 }
